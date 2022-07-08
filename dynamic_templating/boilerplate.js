256992836978c7d1f89441a267269da823c3371d
Vaibhav Jain <vaibhav.jain2472@gmail.com>
2022-07-08 14:53:41 +0530

    removed dynamic templating directory

--- dynamic_templating/boilerplate.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const folderName = `section${process.argv[2]}`;
-// const ok = `Notes_project/section${sectionNo}`;
-
-fs.mkdir(folderName, { recursive: true }, (err) => {
-    if (err) throw err;
-});
-
-fs.writeFileSync(`${folderName}/sec${process.argv[2]}.html`, " ");
